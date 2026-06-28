7426aa26f0d811fea168c635d57b715faa2c08fa
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-06-28 21:32:23 +0530

    Change e-waste to e-wast

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ import { Github, ExternalLink } from 'lucide-react'
 const PROJECTS = [
   {
     title: '♻️ ReVibe',
-    desc: 'An innovative e-waste management platform.',
+    desc: 'An innovative e-wast management platform.',
     ss: 'https://images.unsplash.com/photo-1550989460-0adf9ea622e2?q=80&w=600&auto=format&fit=crop',
     tech: ['React', 'JavaScript', 'Node.js', 'CSS'],
     live: 'https://reviber.netlify.app/',
